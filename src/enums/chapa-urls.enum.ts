bfb2530e4575e3d8e325b71b6cd799d0177f23e9
cher-yeros <yerosendiriba1892@gmail.com>
2024-01-26 23:16:32 +0300

    direct charge incorporated

--- src/enums/chapa-urls.enum.ts
+++ src/enums/chapa-urls.enum.ts
@@ -1,3 +1,7 @@
+// import { DirectChargePaymentMethod } from '../interaces/initialize.interface';
+
+import { DirectChargePaymentMethod } from '../interfaces';
+
 export enum ChapaUrls {
   INITIALIZE = 'https://api.chapa.co/v1/transaction/initialize',
   MOBILE_INITIALIZE = 'https://api.chapa.co/v1/transaction/mobile-initialize',
@@ -5,3 +9,10 @@ export enum ChapaUrls {
   BANKS = 'https://api.chapa.co/v1/banks',
   SUBACCOUNT = 'https://api.chapa.co/v1/subaccount',
 }
+
+export const INITIALIZE_DIRECT_CHARGE = ({ type }: DirectChargePaymentMethod) =>
+  `https://api.chapa.co/v1/charges?type=${type}`;
+
+export const AUTHERIZE_DIRECT_CHARGE = (
+  payment_method: 'telebirr' | 'cbebirr' | 'awash_birr' | 'ebirr' | 'amole'
+) => `https://api.chapa.co/v1/validate?type=${payment_method}`;
